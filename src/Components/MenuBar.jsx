8da879d8f629c0e11cea2ff35034d704b540ccd2
RomiSpring <romina.martinez496@gmail.com>
2026-03-23 09:58:38 -0300

    Hamburguer Menu for mobile

--- src/Components/MenuBar.jsx
+++ src/Components/MenuBar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { assets } from "../assets/assets";
+import{ Menu, X }from 'lucide-react'
 
 const MenuBar = () => {
     const [menuOpen, setMenuOpen] = useState(false);
@@ -18,7 +19,13 @@ const MenuBar = () => {
                 <button className="text-gray-700 hover:text-blue-500 font-medium">
                     Iniciar Sesión
                 </button>
-                <button className="bg-gray-100 hover:bg-gray-200 text-gray-700 font-medium px-4 py-2 rounded-full transition-all">Crear Cuenta</button>
+                <button className="bg-gray-100 hover:bg-red-200 text-gray-700 font-medium px-4 py-2 rounded-full transition-all">Crear Cuenta</button>
+            </div>
+            {/*Hamburguesa mobile | Mobile hamburguer*/}
+            <div className="flex md:hidden">
+                <button onClick={ () => setMenuOpen(!menuOpen)}>
+                    {menuOpen ? <X size={28}/> : <Menu size={28} />}
+                </button>
             </div>
         </nav>
     )
